b1b99c80929ea2359dbe91c51b8761a5aa0d867a
Ankush Goyal <ankush.goyal@nextag.com>
2012-04-05 18:15:03 -0500

    finished

--- bowlingGameKata/java/src/test/BowlingGameTest.java
+++ bowlingGameKata/java/src/test/BowlingGameTest.java
@@ -26,6 +26,15 @@ public class BowlingGameTest {
 			game.roll(pins);
 		}
 	}
+	
+	private void rollSpare() {
+		game.roll(5);
+		game.roll(5);
+	}
+	
+	private void rollStrike() {
+		game.roll(10);
+	}
 
 	@Test
 	public void test_gutterGame() throws Exception {
@@ -39,6 +48,26 @@ public class BowlingGameTest {
 		assertEquals(20,game.score());
 	}
 	
+	@Test
+	public void test_oneSpare() throws Exception {
+		rollSpare();
+		game.roll(3);
+		rollMany(17, 0);
+		assertEquals(16, game.score());
+	}
 	
-
+	@Test
+	public void test_oneStrike() throws Exception {
+		rollStrike();
+		game.roll(3);
+		game.roll(4);
+		rollMany(16, 0);
+		assertEquals(24, game.score());
+	}
+	
+	@Test
+	public void test_perfectGame() throws Exception {
+		rollMany(12, 10);
+		assertEquals(300, game.score());
+	}
 }
